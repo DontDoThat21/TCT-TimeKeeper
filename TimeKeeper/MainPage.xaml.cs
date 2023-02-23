1620987b62a93f1800b6f61bc65000f9013f2206
Tylor Trub <Tylortrub@gmail.com>
2023-02-23 15:56:23 -0500

    Added cool modal time pages. Added more content to main page. Added content to time keeper page but temporary. Meh. Coded delegate style event handlers for days.

--- TimeKeeper/MainPage.xaml.cs
+++ TimeKeeper/MainPage.xaml.cs
@@ -2,7 +2,19 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	int count = 1;
+	public Task<Views.TimeSetterPage> PopModalAsync { get; set; }
+
+    enum Days
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
 
 	public MainPage()
 	{
@@ -21,9 +33,87 @@ public partial class MainPage : ContentPage
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
 
-    private void BtnTime_Clicked(object sender, EventArgs e)
+    private async void BtnTime_Clicked(object sender, EventArgs e)
     {
-
+		await Shell.Current.GoToAsync("TimeSetterPage");	// thanks nick gurfr
+		//await DisplayAlert("Sup", "Fire", "Click me");
     }
+
+    private async void SendRequestToDB_Clicked(object sender, EventArgs e)
+    {
+		await CallNewDBInsert();
+    }
+
+    public async Task CallNewDBInsert()
+    {
+		try
+		{
+            HttpClient cli = new HttpClient();
+            cli.Send(new HttpRequestMessage(HttpMethod.Post, "localhost.com/StressBtn_LeftClicked"));
+            cli.Dispose();
+        }
+		catch (Exception Ex)
+		{
+			await DisplayAlert(Ex.Message, "MainPage", "HAHA LOLU", "Connection error.");
+		}
+		await DisplayAlert("DB Called.", "Sent alert to DB, congrats you just upped the world!", "Test");
+    }
+
+    private async void BtnMonday_Clicked(object sender, EventArgs e)
+    {
+        await SetDayRequest_Handler(sender, e, Days.Monday);
+    }
+
+    private async void BtnTuesday_Clicked(object sender, EventArgs e)
+    {
+        await SetDayRequest_Handler(sender, e, Days.Tuesday);
+    }
+
+    private async void BtnWednesday_Clicked(object sender, EventArgs e)
+    {
+        await SetDayRequest_Handler(sender, e, Days.Wednesday);
+    }
+
+    private async void BtnThursday_Clicked(object sender, EventArgs e)
+    {
+        await SetDayRequest_Handler(sender, e, Days.Thursday);
+    }
+
+    private async void BtnFriday_Clicked(object sender, EventArgs e)
+    {
+        await SetDayRequest_Handler(sender, e, Days.Friday);
+    }
+
+    private async void BtnSaturday_Clicked(object sender, EventArgs e)
+    {
+        await SetDayRequest_Handler(sender, e, Days.Saturday);
+    }
+
+    private async void BtnSunday_Clicked(object sender, EventArgs e)
+    {
+        await SetDayRequest_Handler(sender, e, Days.Sunday);
+    }
+
+    private async Task SetDayRequest_Handler(object sender, EventArgs e, ValueType res)
+    {
+        await SendDayBlockToDB(sender, e, res);
+    }
+
+    private async Task SendDayBlockToDB(object sender, EventArgs e, ValueType res)
+    {
+        string gkapimcypabndianfString = "yarr";
+
+        try
+        {
+            HttpClient cli = new HttpClient();
+            cli.Send(new HttpRequestMessage(HttpMethod.Post, $"localhost.com/SendDayBlockToDB?{gkapimcypabndianfString}"));
+            cli.Dispose();
+        }
+        catch (Exception Ex)
+        {
+            await DisplayAlert(Ex.Message, "MainPage", "HAHA LOLU", "Connection error.");
+        }
+    }
+
 }
 
