5ab41bbde78c35f0436172710531eba21d8a9e8d
Tylor Trub <Tylortrub@gmail.com>
2023-03-16 11:15:34 -0400

    Saving new page logic and foundational time setting grid. This grid/form will delegate most user requests.

--- TimeKeeper/TimeSetterPage.xaml.cs
+++ TimeKeeper/TimeSetterPage.xaml.cs
@@ -2,17 +2,31 @@ using CommunityToolkit.Maui.Views;
 
 namespace TimeKeeper.Views;
 
-public partial class TimeSetterPage : Popup
+public partial class TimeSetterPage : ContentPage
 {
 	public TimeSetterPage()
 	{
 		InitializeComponent();
 	}
 
-	private async void AlertDBOfClick_Clicked(object sender, EventArgs e)
+	private async void Btn_AlertDBOfClickClicked(object sender, EventArgs e)
     {
 		MainPage mp = new MainPage();
 		await Shell.Current.Navigation.PushModalAsync(mp);
-        this.Close();
+    }
+
+    private void Btn_WClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Btn_EveryDayEntryClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Lbl_DayOfWeekGreeterLoad(object sender, EventArgs e)
+    {
+        Lbl_DayOfWeekGreeter.Text = Lbl_DayOfWeekGreeter.Text.Replace("{day}", DateTime.Now.DayOfWeek.ToString());
     }
 }
